e2b4dac6f9a56972eb1d2018c4b39e5615740b58
bvo4 <brandonvo1980@yahoo.com>
2022-04-28 09:45:12 -0400

    Topic searching completed

--- PHP web resources/search.php
+++ PHP web resources/search.php
@@ -20,23 +20,24 @@ echo $header;
 	{
 		$topic = ''; 
 		$subtopic = '';
+		
 		for($b = 0; $b < sizeof($_GET['topic'], 0); $b++)
 		{
 			$temp = substr($_GET['topic'][$b], 0, 4);
 			if($temp == "top_")
 			{
 				$temp = substr($_GET['topic'][$b], 4);
-				$topic .= " " . substr($_GET['topic'][$b], 4);
+				$topic .= "," . substr($_GET['topic'][$b], 4);
 			}
 			else if($temp == "sub_")
 			{
 				$temp = substr($_GET['topic'][$b], 4);
-				$subtopic .= substr($_GET['topic'][$b], 4);
+				$subtopic .= ',' . substr($_GET['topic'][$b], 4);
 			}
 		}
 		
-		$topic = explode(' ', $topic);
-		$subtopic = explode (' ', $subtopic);
+		$topic = explode(',', $topic);
+		$subtopic = explode (',', $subtopic);
 		echo "TOPIC: ";
 		print_r ($topic);
 		echo "<br/>";
@@ -58,7 +59,7 @@ function topic_search($topic, $subtopic)
 {
 	if(isset($topic))
 	{
-		$search_topic = ' and topic.tname in ("' . array_shift($topic) . '"';
+		$search_topic = ' topic.tname in ("' . array_shift($topic) . '"';
 		$tid = $topic;
 		
 		for($b = 0; $b < sizeof($topic, 0); $b++)
@@ -70,25 +71,25 @@ function topic_search($topic, $subtopic)
 	
 	if(isset($subtopic))
 	{
-		$search_subtopic = ' and subtopic.sname in ("' . array_shift($subtopic) . '"';
+		$search_subtopic = ' or subtopic.sname in ("' . array_shift($subtopic) . '"';
 		$tid = $subtopic;
 		
 		for($b = 0; $b < sizeof($subtopic, 0); $b++)
 		{	
-			$search_subtopic.= ", '$subtopic[$b]'";
+			$search_subtopic.= ", ' $subtopic[$b]'";
 		}
 		$search_subtopic.= ')';
 	}
 	include 'db_connection_project.php';
 	$conn = OpenCon();
 	
-	$sql = "select *
+	$sql = "select distinct *
 			from post_question, users, topic, questions, subtopic
 			where questions.qid = post_question.qid 
 			and questions.stid = subtopic.stid
 			and post_question.uid = users.uid 
-			$search_topic
-			$search_subtopic
+			and ($search_topic
+			$search_subtopic)
 			";
 	
 	echo "SQL: " . $sql;
